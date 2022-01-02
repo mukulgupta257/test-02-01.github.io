84f1142e6deaa325117a3c7dfb7b2a288f3491db
Mukul Gupta <mukulgupta257@gmail.com>
2022-01-02 15:24:50 +0530

    completed

---
+++ function.js
@@ -0,0 +1,116 @@
+//first image
+document.getElementById("1").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('1').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("1").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('1').classList.remove("active-image")
+    })
+})
+
+//second image
+document.getElementById("2").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('2').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("2").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('2').classList.remove("active-image")
+    })
+})
+
+//third image
+document.getElementById("3").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('3').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("3").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('3').classList.remove("active-image")
+    })
+})
+
+//fourth image
+document.getElementById("4").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('4').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("4").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('4').classList.remove("active-image")
+    })
+})
+
+//fifth image
+document.getElementById("5").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('5').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("5").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('5').classList.remove("active-image")
+    })
+})
+
+//sixth image
+document.getElementById("6").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('6').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("6").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('6').classList.remove("active-image")
+    })
+})
+
+//seventh image
+document.getElementById("7").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('7').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("7").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('7').classList.remove("active-image")
+    })
+})
+
+//eight image
+document.getElementById("8").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('8').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("8").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('8').classList.remove("active-image")
+    })
+})
+
+//ninth image
+document.getElementById("9").addEventListener('mouseenter',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('9').classList.add("active-image")
+    })
+    console.log("aaya mouse")
+})
+document.getElementById("9").addEventListener('mouseleave',_=>{
+    addEventListener('click',_=>{
+        document.getElementById('9').classList.remove("active-image")
+    })
+})
